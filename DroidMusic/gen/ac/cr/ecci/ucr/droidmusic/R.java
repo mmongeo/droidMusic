97307495f4d118879fab65f9a54c47431f9bbe5b
mario <mario@PHENOM.(none)>
2012-09-09 01:18:31 -0600

    Ya esta el item de musica

--- DroidMusic/gen/ac/cr/ecci/ucr/droidmusic/R.java
+++ DroidMusic/gen/ac/cr/ecci/ucr/droidmusic/R.java
@@ -11,17 +11,24 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int headphones=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int play=0x7f020003;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070002;
+        public static final int item_Artista=0x7f070005;
+        public static final int item_Cancion=0x7f070004;
+        public static final int item_audifono=0x7f070003;
+        public static final int item_play=0x7f070002;
+        public static final int menu_settings=0x7f070006;
         public static final int textView1=0x7f070000;
         public static final int textView2=0x7f070001;
     }
     public static final class layout {
         public static final int activity_lista_canciones=0x7f030000;
         public static final int activity_splash=0x7f030001;
+        public static final int item_cancion=0x7f030002;
     }
     public static final class menu {
         public static final int activity_lista_canciones=0x7f060000;
