8ff9ed02aa423a83b99fd474359c824c68f3fed6
mario <mario@PHENOM.(none)>
2012-09-11 17:38:45 -0600

    Solo falta boton de mas y cambiar que salga el texto de no hay resultados

--- DroidMusic/gen/ac/cr/ecci/ucr/droidmusic/R.java
+++ DroidMusic/gen/ac/cr/ecci/ucr/droidmusic/R.java
@@ -20,13 +20,14 @@ public final class R {
     public static final class id {
         public static final int RelativeLayout1=0x7f070000;
         public static final int boton_buscar=0x7f070002;
-        public static final int item_Artista=0x7f070009;
-        public static final int item_Cancion=0x7f070008;
-        public static final int item_audifono=0x7f070007;
-        public static final int item_estrella=0x7f07000a;
-        public static final int item_play=0x7f070006;
+        public static final int boton_mas=0x7f070006;
+        public static final int item_Artista=0x7f07000a;
+        public static final int item_Cancion=0x7f070009;
+        public static final int item_audifono=0x7f070008;
+        public static final int item_estrella=0x7f07000b;
+        public static final int item_play=0x7f070007;
         public static final int layout_ListaCanciones=0x7f070003;
-        public static final int menu_settings=0x7f07000b;
+        public static final int menu_settings=0x7f07000c;
         public static final int texto_busqueda=0x7f070001;
         public static final int texto_centro=0x7f070004;
         public static final int texto_espere=0x7f070005;
@@ -34,17 +35,20 @@ public final class R {
     public static final class layout {
         public static final int activity_lista_canciones=0x7f030000;
         public static final int activity_splash=0x7f030001;
-        public static final int item_cancion=0x7f030002;
+        public static final int item_boton_mas=0x7f030002;
+        public static final int item_cancion=0x7f030003;
     }
     public static final class menu {
         public static final int activity_lista_canciones=0x7f060000;
         public static final int activity_splash=0x7f060001;
     }
     public static final class string {
+        public static final int Nada=0x7f040007;
         public static final int app_name=0x7f040000;
         public static final int droidMusic=0x7f040004;
         public static final int hello_world=0x7f040001;
         public static final int iniciando=0x7f040005;
+        public static final int mas=0x7f040008;
         public static final int menu_settings=0x7f040002;
         public static final int title_activity_lista_canciones=0x7f040006;
         public static final int title_activity_splash=0x7f040003;
--- DroidMusic/src/ac/cr/ecci/ucr/droidmusic/ui/ListaCancionesActivity.java
+++ DroidMusic/src/ac/cr/ecci/ucr/droidmusic/ui/ListaCancionesActivity.java
@@ -1,23 +1,168 @@
 package ac.cr.ecci.ucr.droidmusic.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 import ac.cr.ecci.ucr.droidmusic.R;
-import ac.cr.ecci.ucr.droidmusic.R.layout;
-import ac.cr.ecci.ucr.droidmusic.R.menu;
+import ac.cr.ecci.ucr.droidmusic.bo.Song;
+
+import ac.cr.ecci.ucr.droidmusic.service.SimpleDroidMusicService;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ListaCancionesActivity extends Activity {
 
+	ListView mListCanciones;
+	ImageButton mButton;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_canciones);
+        mButton = (ImageButton) findViewById(R.id.boton_buscar);
+        mListCanciones = (ListView) findViewById(R.id.layout_ListaCanciones);
+        mListCanciones.setAdapter(new MusicAdapter(this,new ArrayList<Song>()));
+        
+        mButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				TaskSongs task = new TaskSongs();
+				if(((EditText)findViewById(R.id.texto_busqueda)).getText().toString().contentEquals("")){
+					task.setEmpty(true);
+				}
+				else{
+					task.setEmpty(false);
+				}
+				task.execute();
+			}
+		});         
     }
-
+    
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_lista_canciones, menu);
         return true;
     }
+    
+	private static class MusicAdapter extends BaseAdapter {
+		List<Song> songs;
+		LayoutInflater inflater;
+
+		public MusicAdapter(Context context, List<Song> songs) {
+			this.songs = songs;
+			inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		}
+
+		public void addList(List<Song> songs) {
+			this.songs.addAll(songs);
+			notifyDataSetChanged();
+		}
+
+		public int getCount() {
+			return songs.size();
+		}
+
+		public Object getItem(int position) {
+			return songs.get(position);
+		}
+
+		public long getItemId(int position) {
+			return position;
+		}
+
+		public View getView(int position, View convertView, ViewGroup parent) {
+			Song song = (Song) getItem(position);
+
+			SongViewHolder holder = null;
+			if (convertView == null) {
+
+				convertView = inflater.inflate(R.layout.item_cancion,parent, false);
+				holder = new SongViewHolder();
+
+				convertView.setTag(holder);
+
+				holder.artist = (TextView) convertView.findViewById(R.id.item_Artista);
+				holder.title = (TextView) convertView.findViewById(R.id.item_Cancion);
+
+			} else {
+				holder = (SongViewHolder) convertView.getTag();
+			}
+
+			holder.artist.setText(song.getArtist());
+			holder.title.setText(song.getSongName());
+
+			return convertView;
+		}
+	}
+
+	private static class SongViewHolder {
+		public TextView title;
+		public TextView artist;
+		public ImageView photoThumbnail;
+	}
+	
+	 class TaskSongs extends AsyncTask<Void, Void, List<Song>> {
+		boolean empty = false;
+		ProgressDialog progress;
+	
+		public boolean isEmpty() {
+			return empty;
+		}
+
+		public void setEmpty(boolean empty) {
+			this.empty = empty;
+		}
+		
+		
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			progress = ProgressDialog.show(ListaCancionesActivity.this, "Espere", "Buscando canciones...");
+		}
+
+		@Override
+		protected List<Song> doInBackground(Void... params) {
+			List<Song> songs = new ArrayList<Song>();
+			if(empty == false){
+				SimpleDroidMusicService service = (SimpleDroidMusicService) DroidMusicServiceFactory.getInstance();
+				
+				try {
+					songs = service.getSongs(10);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			return songs;
+		}
+
+		@Override
+		protected void onPostExecute(List<Song> result) {
+			
+			MusicAdapter adapter = (MusicAdapter) mListCanciones.getAdapter();
+			adapter.addList(result);
+			progress.dismiss();
+		}
+	};
+    
 }
